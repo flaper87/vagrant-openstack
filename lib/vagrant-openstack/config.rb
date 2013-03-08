65eb15265a0dc0387c29b01c0ddf90ef4b14e5bf
Flaper Fesp <flaper87@gmail.com>
2013-03-08 01:01:43 +0100

    Replace :vm with :name

--- lib/vagrant-openstack/config.rb
+++ lib/vagrant-openstack/config.rb
@@ -2,13 +2,13 @@ module VagrantPlugins
   module ProviderOpenStack
     class Config < Vagrant.plugin("2", :config)
       attr_accessor :url
-      attr_accessor :vm
       attr_accessor :tenant
       attr_accessor :user
       attr_accessor :password
 
 
       attr_accessor :key
+      attr_accessor :name
       attr_accessor :image
       attr_accessor :flavor
       
